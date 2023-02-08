9ec06fe20c2633f5dac1611f5e6f9c3ed984fd35
mhestrada85 <mhestrada85@gmail.com>
2023-02-08 14:02:04 -0700

    using conditionals

--- loops.js
+++ loops.js
@@ -1,37 +1,22 @@
-for (let i = 0; i < 10; i++){
-    console.log(i);
+// create a variable called speedLimit and another called mySpeed
+// set their values to numbers between 1 and 100
+let speedLimit = 50;
+let mySpeed = 65;
+
+
+// using a conditional, determine if mySpeed is greater than the speedLimit
+// if true, print "Slow Down! Mom is mad!" to the console
+// if mySpeed is equal to the speedLimit, print "Everyone is happy!" to the console
+// if mySpeed is less than the speedLimit, print "Speed up! Dad is mad!"
+if(mySpeed > speedLimit){
+    console.log("Slow Down! Mom is mad!")
+} else if(mySpeed == speedLimit){
+    console.log("Everyone is happy!")
+} else {
+    console.log("Speed up! Dad is mad!")
 }
 
-console.log('Whatever is next');
 
-for (let i = 0; i <= 20; i++) {
-    if(i % 2 == 0) {
-        console.log(i);
-    }
-}
 
-let names = ['Sam', 'Tom', 'Hillary'];
 
-for (let i = 0; i < names.length; i++) {
-    console.log(names[i]);
-}
 
-let a = 110
-
-while (a < 10) {
-    console.log(a);
-    a++;
-}
-
-do {
-    console.log(a)
-} while (a < 10)
-
-for (i in names) {
-    console.log(i);
-}
-
-for (name of names) {
-    console.log(name);
-}
-git init
